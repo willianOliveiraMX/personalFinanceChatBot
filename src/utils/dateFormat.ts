5bb1158aba872638d9b7ba7d1e41588f3f80e951
willianOliveiraMX <willianvoliveiravx@gmail.com>
2022-05-08 17:42:27 -0300

    chore: add date log

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,19 +1,17 @@
-import { format } from 'date-fns';
-import { zonedTimeToUtc } from 'date-fns-tz';
+// import { format } from 'date-fns';
+import { format } from 'date-fns-tz';
 import { pt } from 'date-fns/locale';
 
-const pattern = "d/LLLL/yyyy' às 'HH:mm'h'"
+const pattern = "dd/LLLL/yyyy' às 'HH:mm'h'"
 
 export const dateFormat = (date: string) => {
-    const utcDate = zonedTimeToUtc(new Date(date), 'America/Sao_Paulo');
-
-    return format(new Date(utcDate), pattern, { locale: pt });
+    console.log("Date log: ", new Date(), date);
+    return format(new Date(date), pattern, { timeZone: 'America/Sao_Paulo', locale: pt });
 };
 
 export const dateFormatMonthYear = (date: string) => {
-    const utcDate = zonedTimeToUtc(new Date(date), 'America/Sao_Paulo');
-
-    return format(new Date(utcDate), "LLLL 'de' yyyy", { locale: pt });
+    console.log("Date log: ", new Date(), date);
+    return format(new Date(date), "LLLL 'de' yyyy", { timeZone: 'America/Sao_Paulo', locale: pt });
 }
 
 export default dateFormat;
